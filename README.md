4ec90294aff009fb1149d0d192a97b1e9993d482
Ron Bjork <grbtxtmsg@gmail.com>
2014-04-25 16:50:41 -0700

    Added content to README.md and trimmed out inefficientcy in run_analysis.R

--- README.md
+++ README.md
@@ -2,13 +2,37 @@
 
 ## Load text files code
 
+The code begins by loading all the 'txt' files containing the values of computed features of sensor data and all the files that label the features(column names), activityLabels(maps activityCode of activity at time of measurement to activity name) and featureLabels which is the code of the activity that occurred at the time of measurement. Also is computed are the features giving the means and standard deviations values.
+Variable names in code:
+	FeatureFile: files name containing list of features names in data set
+	features: data.frame of features loaded from feature file above
+	featureChar: this is the character vector conversion of features values
+	desiredFeatures: the computed features that are means or standard deviations
+	activityLabels: maps the six activity codes to 6 activity names.
+	subjectTrainLabels: the subject code of the person from which the row feature data is recorded
+	trainLabels: row values of activity labels
+	trainData: data.frame containing the features values computed from sensor data
+
+After trainData and labels are loaded the desired features(mean and std's) are selected from trainData using the desiredFeatures character vector, and trainData is reassigned to this reduced columns dataset.
+
 
 ## Adding labels code
+	Columns of activityCode and subjectCode are grafted onto trainData.
+	Activity labels are then merged into the trainData using the activityCode above and activityLabels data.
 
-
+## Test Data is loaded:
+	Train data is loaded in manner identical to trainData above
+	
 ## Merging training and test sets together
-
+	The test data is appended to training data and assigned to variable "appendedData". 
 
 ## Computing the mean valuse of each subjects activity features
+	Within the nested for loops the means are computed for the featues of each subjects activity.
+	The begining of the outer for loop selects rows of the dataSet "appenedData" belonging the currently indexed subject. The the innner loops selects all the rows with a recording for a particular activity type and computes the mean - used R's 'colMeans method. 
+	The data.frame 'meanActivityValues' is used to accumulate the values
 
 ## outputing tidy data.
+	After all the means are computed the columns are reordered so that subject and and activity name are the first two columns of the data set.
+
+## Write out to 'csv' file
+	The data set is written out to 'tidy.csv'.
--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,36 @@
 # Clean Data Assignment 
 # Code written by Ronald G. Bjork
 
-
+# Loading of Feature data
 featuresFile <- "./features.txt"
 features <- read.table(featuresFile)
-featureChar = as.character(features[,2])
+featureChar = as.character(features[,2]) # character vector of feature names
+
+# Assemble names of columns that have 'std' or 'mean' in them. These are the columns containing means and standard deviations of features
+# These are the name of the features which the assignment has requested be extracted ans assembled
 desiredFeatures <- str_detect(as.character(features[,2]),"(-std)|(-mean[^F])") 
 
-activityLabels <- read.table("./activity_labels.txt",col.names=c("activityCode","activityName")) # Merge with tainData
+# Load in activity table that associates activityCode with activityName.
+activityLabels <- read.table("./activity_labels.txt",col.names=c("activityCode","activityName")) 
 
 trainPath <- "./train/";
 
+# Load the id's of people(subjects) that wore the samsung phone and recorded there movements
 subjectTrainLabels <- read.table("./train/subject_train.txt")
+
+# Load in computed features of sensor data and assign column names - 
+# The training data columns are assumed to be in the same order as the list of names in features
 trainLabels <- read.table(paste(trainPath,"y_train.txt",sep=""),col.names=c("Activity"))
 trainData <- read.table(paste(trainPath,"x_train.txt",sep=""), col.names=featureChar)
+# The features of interest, desiredFeatures(computed above), are extracted and other features discarded.
+# This results in a reduction in the used columns in table
 trainData <- trainData[,desiredFeatures]
 
 
+# Adds column for activityCode
 trainData$activityCode <- trainLabels[,1]
 trainData$subjectCode <- subjectTrainLabels[,1]
+# Merge into table activity names using activityCode id's in both trainData(added above) and activitylabels
 mergedTrainingData = merge(trainData, activityLabels,by.x="activityCode",by.y="activityCode",all=TRUE)
 
 testPath <- "./test/";
@@ -31,17 +43,19 @@ testData$activityCode <- testLabels[,1]
 testData$subjectCode <- subjectTestLabels[,1]
 mergedTestData = merge(testData, activityLabels,by.x="activityCode",by.y="activityCode",all=TRUE)
 
-appendedDataSets <- rbind(mergedTrainingData,mergedTestData)
+# Training and Test data is aggregated - trining data first followed by rows of test data
+appendedDataSets <- rbind(mergedTrainingData,mergedTestData) # result meeting requirements 1,2,3,4
 
 #
 # Create new tidy dataset - requirement #5
 #
 colNames <- names(appendedDataSets)
 columnsToAvg <- !str_detect(colNames,"(activityName)|(activityCode)|(subjectCode)") # Bool array to select all but these three columns
+# OR
 #columnsToAvg <- -grep("(activityName)|(activityCode)|(subjectCode)",colNames)
 
-NSubjects = 30
-NActivities = 6
+NSubjects = 30 # Number of subjects
+NActivities = 6 # Number of activity types 
 first = TRUE
 for(i in 1:NSubjects){
   subject <- appendedDataSets[appendedDataSets$subjectCode == i,]
@@ -50,17 +64,13 @@ for(i in 1:NSubjects){
     activities <- subject[subject$activityCode == j,]
     if(dim(activities)[1] == 0)next
     activityName <- activities[1,]$activityName
+    colmeans <- data.frame(matrix(colMeans(activities[,columnsToAvg]),nrow=1))
+    colnames(colmeans) = colNames[columnsToAvg]
+    colmeans$Activity_Name = activityName;
+    colmeans$Subject_Code = subjectCode;
     if(first){
-      colmeans <- data.frame(matrix(colMeans(activities[,columnsToAvg]),nrow=1))
-      colnames(colmeans) = colNames[columnsToAvg]
-      colmeans$Activity_Name = activityName;
-      colmeans$Subject_Code = subjectCode;
       meanActivityValues <- colmeans
     }else{
-      colmeans <- data.frame(matrix(colMeans(activities[,columnsToAvg]),nrow=1))
-      colnames(colmeans) = colNames[columnsToAvg]
-      colmeans$Activity_Name = activityName;
-      colmeans$Subject_Code = subjectCode;
       meanActivityValues <- rbind(meanActivityValues,colmeans)
     }
     first = FALSE
